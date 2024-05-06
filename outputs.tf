895861dbf2647637f18f0627450b3b01979e1cd4
GitHub Action <notifications@github.com>
2024-05-06 19:14:40 +0000

    Automated Terraform Formatting

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "project_id" {
   description = "The project ID."
-  value       = {for key, value in tfe_project.this: key => value.id}
+  value       = { for key, value in tfe_project.this : key => value.id }
 }
 
 output "team" {
   description = "The team resources attributes."
-  value       = {for key, value in tfe_team.this: key => value}
+  value       = { for key, value in tfe_team.this : key => value }
 }
 
 output "team_id" {
   description = "The ID of the team."
-  value       = {for key, value in tfe_team.this: key => value.id}
+  value       = { for key, value in tfe_team.this : key => value.id }
 }
 
 output "team_project_access" {
   description = "The team project access attributes."
-  value       = {for key, value in tfe_team_project_access.this: key => value.id}
+  value       = { for key, value in tfe_team_project_access.this : key => value.id }
 }
