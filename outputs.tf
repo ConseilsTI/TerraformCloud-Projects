6da5a3b2f97713ed77cd1917867c0640f4d3e1ab
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-05-06 15:10:53 -0400

    v1.0.0

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,19 @@
-# output "repository" {
-#   description = "Repositories within your GitHub organization."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository }
-# }
+output "project_id" {
+  description = "The project ID."
+  value       = {for value in tfe_project.this: value.key => value.id}
+}
 
-# output "full_name" {
-#   description = "A string of the form \"orgname/reponame\"."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository.full_name }
-# }
+output "team" {
+  description = "The team resources attributes."
+  value       = {for value in tfe_team.this: value.key => value}
+}
 
-# output "html_url" {
-#   description = "URL to the repository on the web."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository.html_url }
-# }
+output "team_id" {
+  description = "The ID of the team."
+  value       = {for value in tfe_team.this: value.key => value.id}
+}
 
-# output "ssh_clone_url" {
-#   description = "URL that can be provided to git clone to clone the repository via SSH."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository.ssh_clone_url }
-# }
-
-# output "http_clone_url" {
-#   description = "URL that can be provided to git clone to clone the repository via HTTPS."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository.http_clone_url }
-# }
-
-# output "git_clone_url" {
-#   description = "URL that can be provided to git clone to clone the repository anonymously via the git protocol."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository.git_clone_url }
-# }
-
-# output "svn_url" {
-#   description = "URL that can be provided to svn checkout to check out the repository via GitHub's Subversion protocol emulation."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository.svn_url }
-# }
-
-# output "node_id" {
-#   description = "GraphQL global node id for use with v4 API."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository.node_id }
-# }
-
-# output "repo_id" {
-#   description = "GitHub ID for the repository."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository.repo_id }
-# }
-
-# output "primary_language" {
-#   description = "The primary language used in the repository."
-#   value       = { for github_repository in github_repository.this : github_repository.name => github_repository.primary_language }
-# }
+output "team_project_access" {
+  description = "The team project access attributes."
+  value       = {for value in team_project_access.this: value.key => value.id}
+}
--- versions.tf
+++ versions.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/tfe"
       version = "~>0.51"
     }
-    github = {
-      source  = "integrations/github"
-      version = "~>5.44"
-    }
     hcp = {
       source  = "hashicorp/hcp"
       version = "~>0.76"
